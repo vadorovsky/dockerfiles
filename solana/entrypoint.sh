6c1cd0efb360570279f8c2ebab2cb86e2bd54bb7
Michal Rostecki <vadorovsky@protonmail.com>
2023-05-22 20:53:01 +0200

    Remove Light Protocol and solana images
    They moved to github.com/Lightprotocol/dockerfiles

--- solana/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-solana config set --url http://localhost:8899
-
-exec /usr/local/bin/solana-test-validator \
-    --bpf-program noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV /usr/local/lib/solana-program-library/spl_noop.so \
-    "$@"
